4e63895b517736f6cf48730e45204c86ddfef8b5
oacar <omeracar0@gmail.com>
2016-08-10 16:03:47 +0300

    Update test_umbrella_potential.py

--- test/test_umbrella_potential.py
+++ test/test_umbrella_potential.py
@@ -3,6 +3,7 @@
 import numpy as np
 from umbrella_sampling.umbrella_potential import UmbrellaPotential
 from numpy.testing import assert_almost_equal
+from umbrella_sampling.umbrella_iterator import UmbrellaIterator
 
 def test_randomised_sanity_check():
     traj = np.random.rand(1000)
@@ -15,3 +16,14 @@ def test_randomised_sanity_check():
     assert_almost_equal(up.center_correction, 0.0, decimal=16)
     assert_almost_equal(up.slope, 0.0, decimal=16)
 
+def test_new_center_pos():
+	givencenter=np.random.random()
+	spring_const=np.random.randint(1,10)
+	traj=np.random.rand(100)
+	us=UmbrellaPotential(givencenter, spring_const)
+	us.add_trajectory(traj)
+	overlap=np.random.random()
+	s=UmbrellaIterator(us, overlap)
+	newcenter=us.center_correction+us.center
+	s.center_check()
+	assert_almost_equal(newcenter,s.umbrella.center,2)
